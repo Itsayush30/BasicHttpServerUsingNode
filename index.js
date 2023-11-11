badbf1ca1a21ba93951d81b937fc0697dee901da
itsayush30 <itsayush30@gmail.com>
2023-11-11 23:40:05 +0530

    Middleware

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const PORT = 3000;
 
-const app = express(); // create a express server object
+const app = express(); // create a express server object 
 
-app.get('/home', (request, response) => {
+const mylogger = (req, res, next) => {
+    console.log("logging from middleware 1");
+    next();// calls the next middleware
+}
+
+const extlogger = (req, res, next) => {
+    console.log("logging from middleware 2");
+    next();// calls the next middleware
+}
+
+app.get('/home', mylogger. extlogger, (request, response) => {
     // response.send("hi there, welcome to get");
+    console.json("last middleware")
     response.json({
         message: "OK get"
     })
